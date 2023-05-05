df3380dde05f1983e94576403d9326d3a016808f
Gabriel Teofilo <Gabrielteofilo2016@outlook.com>
2023-05-05 01:26:29 -0300

    Home paga nao finalizada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,26 @@
 <html>
     <head>
-        <title>Home - Projetos</title>
+        <title>Home</title>
+        <link rel="icon" type="image/png" href="{{url('img/714043.png')}}"/>
+        <link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}"/>
+        <link rel="stylesheet" type="text/css" href="{{url('css/home.css')}}"/>
     </head>
     <body>
+        <div>
+        <div>
+            <h1>PROJETOS DE DESENVOLVIMENTO</h1>
+            <h4>Projetos desenvolvido por mim para testes e treinamento.</h4>
+        </div>
+        <div>
+            <a href="#" class="animated-button1">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Ver meus Projetos
+            </a>
+        </div>
+        </div>
         <script src="{{url('js/VantaJS/three.r134.min.js')}}"></script>
         <script src="{{url('js/VantaJS/vanta.net.min.js')}}"></script>
         <script src="{{url('js/VantaJS/vanta.js')}}"></script>
