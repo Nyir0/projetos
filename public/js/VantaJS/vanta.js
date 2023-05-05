e3b0a509f3551c06843a1516854c5fdc77bce34a
Gabriel Teofilo <Gabrielteofilo2016@outlook.com>
2023-05-05 00:09:12 -0300

    Vanta JS adicionado

---
+++ public/js/VantaJS/vanta.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+    el: "html",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x271dc7,
+    backgroundColor: 0x1f1339
+})
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Home - Projetos</title>
+    </head>
+    <body>
+        <script src="{{url('js/VantaJS/three.r134.min.js')}}"></script>
+        <script src="{{url('js/VantaJS/vanta.net.min.js')}}"></script>
+        <script src="{{url('js/VantaJS/vanta.js')}}"></script>
+    </body>
+</html>
